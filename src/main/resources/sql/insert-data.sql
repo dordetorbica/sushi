d7ff21bc80bb2eca7318e66e386982a4e492e251
doury <doury@NB8615.office.ctbto.org>
2017-04-06 15:04:18 +0200

    updated insert data 2nd attempt

--- src/main/resources/sql/insert-data.sql
+++ src/main/resources/sql/insert-data.sql
@@ -3,5 +3,5 @@ INSERT INTO user(username, name, email, pw) VALUES ('dorde','Dorde Torbica','dor
 INSERT INTO user(username, name, email, pw) VALUES ('helmuth','Helmuth Breitenfellner','helmuth.breitenfellner@ctbto.org','helmuth');
 INSERT INTO user(username, name, email, pw) VALUES ('Cahya','Cahya Wirawam','cahya.wirawam@ctbto.org','');
 
-INSERT INTO bet(initiator_id, title, description, pub_date) VALUES (1, 50,'Donald Trump bet', 'Who bets Donald Trump will remain president until the end of his term?', TIMESTAMP '2017-04-06 02:00:22');
-INSERT INTO bet(initiator_id, challenger_id, title, description, pub_date) VALUES (1, 2, 20, 'Winning Scrum Team bet', 'Who bets Team sushi will win this contest?', TIMESTAMP '2017-04-06 02:00:22');
+INSERT INTO bet(initiator_id, title, description, pub_date) VALUES (100, 'Donald Trump bet', 'Who bets Donald Trump will remain president until the end of his term?', TIMESTAMP '2017-04-06 02:00:22');
+INSERT INTO bet(initiator_id, challenger_id, title, description, pub_date) VALUES (100, 101, 'Winning Scrum Team bet', 'Who bets Team sushi will win this contest?', TIMESTAMP '2017-04-06 02:00:22');
