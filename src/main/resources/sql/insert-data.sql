ef259aacb889cbe2f022f0e9940df83d4b4f480c
doury <doury@NB8615.office.ctbto.org>
2017-04-06 15:01:36 +0200

    Insert data updated

--- src/main/resources/sql/insert-data.sql
+++ src/main/resources/sql/insert-data.sql
@@ -1,7 +1,7 @@
-INSERT INTO user(user_id, username, email, pw) VALUES (1, 'user001','user001@email.com','$2a$10$IHdRfnhNgQesPFD5hrUcMOvyx5RrRcklkpXfs9YX4j1qXvouEeVIa');
-INSERT INTO user(user_id, username, email, pw) VALUES (2, 'user002','user002@email.com','$2a$10$NlU0bdBUiegZWZvl6CGpj.wV5YfbDGZ8lYznxWp2NNE4F9cYJJFOe');
-INSERT INTO user(user_id, username, email, pw) VALUES (3, 'user003','user003@email.com','$2a$10$gwEVdI6lSDrkIkGLrsHTIOzLEgnT3gDUDYMOfxvOdnoqvGpf275fm');
-INSERT INTO user(user_id, username, email, pw) VALUES (4, 'user004','user004@email.com','$2a$10$W2ZJXI00xhL03vwcy2Y/DeZe.BqMf4dUSP5lxEQFAqV.ocbUAYS4m');
+INSERT INTO user(username, name, email, pw) VALUES ('admin','Administrator','admin@email.com','admin');
+INSERT INTO user(username, name, email, pw) VALUES ('dorde','Dorde Torbica','dorde.torbica@ctbto.org','dorde');
+INSERT INTO user(username, name, email, pw) VALUES ('helmuth','Helmuth Breitenfellner','helmuth.breitenfellner@ctbto.org','helmuth');
+INSERT INTO user(username, name, email, pw) VALUES ('Cahya','Cahya Wirawam','cahya.wirawam@ctbto.org','');
 
-INSERT INTO bet(initiator_id, amount_eur, description, pub_date) VALUES (1, 50, 'Who bets Donald Trump will remain president until the end of his term?', TIMESTAMP '2017-04-06 02:00:22');
-INSERT INTO bet(initiator_id, amount_eur, description, pub_date) VALUES (2, 20, 'Who bets Team sushi will win this contest?', TIMESTAMP '2017-04-06 02:00:22');
+INSERT INTO bet(initiator_id, title, description, pub_date) VALUES (1, 50,'Donald Trump bet', 'Who bets Donald Trump will remain president until the end of his term?', TIMESTAMP '2017-04-06 02:00:22');
+INSERT INTO bet(initiator_id, challenger_id, title, description, pub_date) VALUES (1, 2, 20, 'Winning Scrum Team bet', 'Who bets Team sushi will win this contest?', TIMESTAMP '2017-04-06 02:00:22');
